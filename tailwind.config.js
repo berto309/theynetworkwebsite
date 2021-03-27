059fc8505690a76cf45bea4760988b36b11d6cc4
Robert Ntim <ntimrobert309@gmail.com>
2021-03-27 03:53:18 +0000

    landing,login,forgot-password

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,14 +14,20 @@ module.exports = {
         'lightblue': colors.lightBlue,
         'orange': colors.orange,
         'amber': colors.amber,
-        'brand': '#181818'
+        'brand': '#181818',
+        'theynetwork':'#C11818'
       }
     },
+    transitionTimingFunction: {
+      'bloop': "cubic-bezier(1,-0.65,0,2.31)"
+    }
   },
   variants: {
    
     extend: {
        backgroundColor:['active'],
+       display:['hover'],
+       scale:['group-hover']
     },
   },
   plugins: [
